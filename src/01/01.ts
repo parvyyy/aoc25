5ff5f20a6e59ab76db1f7d9224779bc88c345a0b
Parv Rehan <parvyyy@Mac.hub>
2025-12-01 20:27:47 +1100

    Completed P1 Day 1

--- src/01/01.ts
+++ src/01/01.ts
@@ -8,5 +8,37 @@ const getData = async () => {
     return data
 }
 
+const Part1 = (data: string) => {
+    const rotations = data.split('\n');
+
+    let dial_delta = 50
+    let count = 0
+    for (const rotation of rotations) {
+        const dir = rotation[0]
+        const amnt = parseInt(rotation.slice(1))
+
+        switch (dir) {
+            case "L":
+                dial_delta -= amnt
+                break
+            case "R":
+                dial_delta += amnt
+                break
+            
+            // Nonsensical value.
+            default:
+                console.log(`ERROR Invalid dir: ${dir}`)
+                return -1
+        }
+
+        count += Number(dial_delta % 100 == 0)
+    }
+
+    return count
+}
+
 const data = await getData()
-console.log(data)
+
+// ans1: 1165
+const ans1 = Part1(data)
+console.log(ans1)
