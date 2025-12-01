923ee15e6c5d4aeb37f43f48614940f2341e87f1
Parv Rehan <parvyyy@Mac.hub>
2025-12-02 02:49:57 +1100

    Completed D6 P2 Problem from 2023 AOC

--- src/misc/06_2023/06.ts
+++ src/misc/06_2023/06.ts
@@ -48,8 +48,40 @@ const Part1 = (data: string) => {
     return prod
 }
 
+// Suprisingly, a more simple version of Part 1, only handling one distance
+// and corresponding time.
+const Part2 = (data: string) => {
+    const [time, distance] = data.split('\n')
+
+    if (!time || !distance) {
+        return null
+    }
+
+    const dist_re = distance?.match(/Distance:\s*(.*)/)
+    const time_re = time?.match(/Time:\s*(.*)/)
+
+    if (!dist_re || !time_re) {
+        return null
+    }
+
+    const dist = parseInt(dist_re[1]?.replaceAll(' ', '') ?? '')
+    const t = parseInt(time_re[1]?.replaceAll(' ', '') ?? '')
+
+    for (let k = 0; k < t; k++) {
+        // All values between `k` and `n-k` are valid due
+        // to symmetry.
+        if (k * (t - k) > dist) {
+            return (t + 1 - (2 * k))
+        }
+    }
+}
+
 const data = await getData()
 
-// ans1: 1165
+// ans1: 303600
 const ans1 = Part1(data)
 console.log(ans1)
+
+// ans2: 23654842
+const ans2 = Part2(data)
+console.log(ans2)
